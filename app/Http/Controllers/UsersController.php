bf6c407be6faef3f614f04bb11bfba3168db7ad1
Eroslone <3079758191@qq.com>
2022-05-30 09:45:25 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,7 +16,7 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
-    public function store(Request $request)
+/*    public function store(Request $request)
     {
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
@@ -24,5 +24,21 @@ class UsersController extends Controller
             'password' => 'required|confirmed|min:6'
         ]);
         return;
+    }*/
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+        return redirect()->route('users.show', [$user]);
     }
 }
