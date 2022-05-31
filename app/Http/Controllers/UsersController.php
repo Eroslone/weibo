ffff7d3ea5e9c37bfd5fb74f3accf80befe1cde8
Eroslone <3079758191@qq.com>
2022-05-31 09:46:50 +0000

    移除邮件发送的from选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -44,13 +44,11 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'Eros@example.com';
-        $name = 'Eros';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message-->to($to)->subject($subject);
         });
     }
     public function confirmEmail($token)
