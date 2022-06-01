9ab4b47e00bd9ccaf77163a003797e4de5b4f913
Eroslone <3079758191@qq.com>
2022-06-01 03:50:02 +0000

    关注和粉丝列表页

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name(
 Route::post('password/reset',  'PasswordController@reset')->name('password.update');
 //微博创建和删除
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+//关注列表、粉丝列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
