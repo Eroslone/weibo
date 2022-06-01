4871c4d64ae18d8a4f9204e66fcd0c5ba598dc19
Eroslone <3079758191@qq.com>
2022-06-01 01:54:06 +0000

    微博数据模型

--- app/Http/Controllers/PasswordController.php
+++ app/Http/Controllers/PasswordController.php
@@ -19,10 +19,10 @@ class PasswordController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('throttle:2,1', [
+        $this->middleware('throttle:6,1', [
             'only' => ['showLinkRequestForm']
         ]);
-        $this->middleware('throttle:3,10', [
+        $this->middleware('throttle:5,10', [
             'only' => ['sendResetLinkEmail']
         ]);
     }
